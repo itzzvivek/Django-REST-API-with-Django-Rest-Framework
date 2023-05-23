87716d687f08f64ed1664588aff943df8c63bc2a
vivek <vivekwarkade000@gmail.com>
2023-05-23 17:33:45 +0530

    listview & listcreateview

--- backend/products/views.py
+++ backend/products/views.py
@@ -3,6 +3,22 @@ from rest_framework import generics
 from .models import Product
 from .serializers import ProductSerializer
 
+class ProductListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    def perform_create(self,serializer):
+        #serializer.save(user=self.request.user)
+        # print(serializer.validated_data)
+        title = serializer.validated_data.get('title')
+        content = serializer.validated_data.get('content')
+        # or None
+        if content is None:
+            content = title
+        serializer.save(content=content)
+        #send a django signals
+product_list_create_view = ProductListCreateAPIView.as_view()
+
 
 class ProductDetailAPIView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
@@ -10,3 +26,13 @@ class ProductDetailAPIView(generics.RetrieveAPIView):
     #lookup_field = 'pk'
 
 product_detail_view = ProductDetailAPIView.as_view()
+
+class ProductListAPIView(generics.RetrieveAPIView):
+    '''
+    not gonna use this method
+    '''
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    #lookup_field = 'pk'
+
+product_list_view = ProductListAPIView.as_view()
