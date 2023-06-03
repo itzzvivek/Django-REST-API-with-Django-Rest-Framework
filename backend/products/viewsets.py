7d66b7c8de34676f3fd4e781155a05e27699897a
vivek <vivekwarkade000@gmail.com>
2023-06-03 16:57:30 +0530

    viewsets & routers

---
+++ backend/products/viewsets.py
@@ -0,0 +1,27 @@
+from rest_framework import mixins,viewsets
+
+from .models import Product
+from .serializers import ProductSerializer
+
+class ProductViewSet(viewsets.ModelViewSet):
+    # get -> list-> queryset
+    # get -> retrieve -> Product Instance Detail view
+    # post -> create -> New Instance
+    # put -> Update
+    # patch -> Partial Update
+    # delete -> destroy
+    
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = "pk" # default
+
+class ProductGenericViewSet(mixins.ListModelMixin,mixins.RetrieveModelMixin,viewsets.GenericViewSet):
+    # get -> list-> queryset
+    # get -> retrieve -> Product Instance Detail view
+
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = "pk" # default
+
+product_list_view = ProductGenericViewSet.as_view({'get':'list'})
+product_detail_view = ProductGenericViewSet.as_view({'get':'retrieve'})
